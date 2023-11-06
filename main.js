c97853926499db8d9a1613768e1a138f7848231d
mixaeljaquisom <141679676+mixaeljaquisom@users.noreply.github.com>
2023-11-06 17:33:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function tocarTeclaGrilo(){
-       document.querySelector ('#som_tecla_grilo').play
+function tocaSomAplausos(){
+    document.querySelector("#som_tecla_aplausos").play();   
+
+document.querySelectorAll('.tecla');
+}
+
+const ListaDeTeclas = document. querySelectorAll('.tecla');
+ListaDeTeclas[2].onclick = tocaSomAplausos
+
+let contador = 0;
+//Estrutura de repetição - Enquanto
+while(contador<9){
+    ListaDeTeclas[0].onclick =
+    contador = 1;
+
+
 }
